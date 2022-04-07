0b30ba0275f695b075de9e7a35bea4f1fe33b739
erikocruz <eriko.cruz@unah.hn>
2022-04-07 13:13:46 -0600

    Tarea #8: Aplicando seguridad a su proyecto web admin.

--- MorsodItalia/MorsodItalia.BL/DatosdeInicio.cs
+++ MorsodItalia/MorsodItalia.BL/DatosdeInicio.cs
@@ -15,8 +15,33 @@ namespace MorsodItalia.BL
             var nuevoUsuario = new Usuario();
             nuevoUsuario.Nombre = "admin";
             nuevoUsuario.Contrasena = Encriptar.CodificarContrasena("123");
-
             contexto.Usuarios.Add(nuevoUsuario);
+
+            var Usuario1 = new Usuario();
+            Usuario1.Nombre = "maryelaa";
+            Usuario1.Contrasena = Encriptar.CodificarContrasena("456");
+            contexto.Usuarios.Add(Usuario1);
+
+            var Usuario2 = new Usuario();
+            Usuario2.Nombre = "adrianp";
+            Usuario2.Contrasena = Encriptar.CodificarContrasena("789");
+            contexto.Usuarios.Add(Usuario2);
+
+            var Usuario3 = new Usuario();
+            Usuario3.Nombre = "josem";
+            Usuario3.Contrasena = Encriptar.CodificarContrasena("012");
+            contexto.Usuarios.Add(Usuario2);
+
+            var Usuario4 = new Usuario();
+            Usuario4.Nombre = "manuels";
+            Usuario4.Contrasena = Encriptar.CodificarContrasena("345");
+            contexto.Usuarios.Add(Usuario4);
+
+            var Usuario5 = new Usuario();
+            Usuario5.Nombre = "erikoc";
+            Usuario5.Contrasena = Encriptar.CodificarContrasena("678");
+            contexto.Usuarios.Add(Usuario5);
+
 
             base.Seed(contexto);
         }
